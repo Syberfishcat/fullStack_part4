8d0c9bf9f5ad66684fd12d5c8d9a57d40ede889c
sybercat <sybercat@163.com>
2026-08-17 21:30:00 +0300

    add test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((pre, cur) => {
+    return pre + cur.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
