4c1355ad64be00b77d96a349bdaea3f3e8959209
sybercat <sybercat@163.com>
2026-08-17 10:10:16 +0300

    complete ex4.2

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+
+app.use(express.json())
+mongoose.connect(config.mongoUrl, { family: 4 })
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
