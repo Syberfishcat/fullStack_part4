208c2b5ad90061f6357b01764b2991a65e4348e7
sybercat <sybercat@163.com>
2026-08-20 10:17:47 +0300

    add test environment

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,66 +1,24 @@
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 5,
-    __v: 0
-  }
-]
-const blogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
-    _id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-    __v: 0
-  },
-  {
-    _id: "5a422b891b54a676234d17fa",
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-    __v: 0
-  },
-  {
-    _id: "5a422ba71b54a676234d17fb",
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-    __v: 0
-  },
-  {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-    __v: 0
-  }  
-]
+// const supertest = require('supertest')
+// const app = require('../app')
+const helper = require('./test_helper')
+
+// const api = supertest(app)
+
+// test('blogs are returned as json', async () => {
+//   await api
+//     .get('/api/blogs')
+//     .expect(200)
+//     .expect('Content-Type', /application\/json/)
+// })
+
+// test('all blogs are returned', async () => {
+//   const response = await api.get('/api/blogs')
+
+//   assert.strictEqual(response.body.length, helper.initialNotes.length)
+// })
 
 test('dummy returns one', () => {
   const result = listHelper.dummy([])
@@ -74,12 +32,12 @@ describe('total likes', () => {
   })
 
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
+    const result = listHelper.totalLikes(helper.listWithOneBlog)
     assert.strictEqual(result, 5)
   })
 
   test('of a bigger list is calculated right', () => {
-    const result = listHelper.totalLikes(blogs)
+    const result = listHelper.totalLikes(helper.initialBlogs)
     assert.strictEqual(result, 36)
   })
 })
@@ -91,13 +49,13 @@ describe('favorite blog', () => {
   }) 
 
   test('of a list has only one blog, equals the only blog', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog)
-    assert.deepStrictEqual(result, listWithOneBlog[0])
+    const result = listHelper.favoriteBlog(helper.listWithOneBlog)
+    assert.deepStrictEqual(result, helper.listWithOneBlog[0])
   })
 
   test('of a bigger lis is caclulated right', () => {
-    const result = listHelper.favoriteBlog(blogs)
-    assert.deepStrictEqual(result, blogs[2])
+    const result = listHelper.favoriteBlog(helper.initialBlogs)
+    assert.deepStrictEqual(result, helper.initialBlogs[2])
   })
 })
 
@@ -108,7 +66,7 @@ describe('author with most blogs', () => {
   })
 
   test('when list has only one blog, returns its author and one blog', () => {
-    const result = listHelper.mostBlogs(listWithOneBlog)
+    const result = listHelper.mostBlogs(helper.listWithOneBlog)
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       blogs: 1
@@ -116,7 +74,7 @@ describe('author with most blogs', () => {
   })
 
   test('of a bigger list is calculated right', () => {
-    const result = listHelper.mostBlogs(blogs)
+    const result = listHelper.mostBlogs(helper.initialBlogs)
     assert.deepStrictEqual(result, {
       author: 'Robert C. Martin',
       blogs: 3
@@ -131,7 +89,7 @@ describe('author with most likes', () => {
   })
 
   test('when list has only one blog, returns its author and likes', () => {
-    const result = listHelper.mostLikes(listWithOneBlog)
+    const result = listHelper.mostLikes(helper.listWithOneBlog)
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       likes: 5
@@ -139,7 +97,7 @@ describe('author with most likes', () => {
   })
 
   test('of a bigger list is calculated right', () => {
-    const result = listHelper.mostLikes(blogs)
+    const result = listHelper.mostLikes(helper.initialBlogs)
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       likes: 17
